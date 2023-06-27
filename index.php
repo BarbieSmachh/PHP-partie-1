e6a917cd0e501deea1d161eabb15e82df9efcbba
BarbieSmachh <hicham.-sebbar@hotmail.fr>
2023-06-27 16:58:03 +0200

    lame as fuck

--- index.php
+++ index.php
@@ -1,6 +1,154 @@
 <?php
+// $name = 'php jaimepas';
+// $lastName = 'Jean';
+// $firstName = 'Eude';
+// $age = 99;
+// echo "<p>$firstName $lastName $age</p>";
 
+// $km = 125;
+// echo "<p>$km</p>";
+// $string = 'ficelle';
+// $int = 99;
+// $float = 99.9;
+// $boolean = true;
+// echo "<p>$string $int $float $boolean</p>";
 
-echo 'dfgsdgfdsg';
+// $int2 = null;
+// echo "<p>$int2</p>";
+// $int2 = 713705;
+// echo "<p>$int2</p>";
+
+// echo "<p>Bonjour $name, comment vas-tu</p>";
+
+// echo "<p>Bonjour $lastName $firstName, tu as $age ans.</p>";
+ 
+// $addition = 3+4;
+// $multiplication = 5*20;
+// $division = 45/5;
+// echo "$addition $multiplication $division";
+
+// if($age>17){
+//     echo "<p>Vous êtes majeur.</p>";
+// }else {
+//    echo "<p>vous êtes mineur.</p>";
+// };
+// $isEasy = true;
+// if($isEasy===true){
+//     echo "<p>C'est facile!!</p>";
+// }else {
+//    echo "<p>C'est difficile !!!</p>";
+// };
+// $gender = "Homme";
+// if($gender==="Homme"&$age>17){
+//     echo "<p>Vous êtes un Homme et vous êtes majeur</p>";
+// }else if($gender!=="Homme"&$age>17){
+//     echo "<p>Vous êtes une Femme et vous êtes majeur</p>";
+// }else if($gender==="Homme"&$age<17){
+//     echo "<p>Vous êtes un Homme et vous êtes mineur</p>";
+// }else {
+//    echo "Vous êtes une Femme et vous êtes mineur";
+// };
+// $tabRichter = ['Micro-séisme impossible à ressentir.','	Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.','Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.','Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.','Séisme capable d\'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes.','	Fort séisme capable d\'engendrer des destructions majeures sur une large distance (180 km) autour de l\'épicentre.','Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.','Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres.','Séisme capable de tout détruire sur une très vaste zone.'];
+// $magnitude = 9;
+//  echo ($tabRichter[$magnitude-1]);
+//  if($gender!=="Homme"){
+//     echo "<p>C'est une développeuse !!!</p>";
+// }else if($gender==="Homme"){
+//     echo "<p>C'est un développeur !!!</p>";
+// };
+// if($age>17){
+//     echo "<p>Tu es majeur</p>";
+// }else{
+//     echo "<p>Tu n'es pas majeur</p>";
+// };
+// $isOk = true;
+// if($isOk == false){
+//     echo "<p>C'est pas bon !!!</p>";
+// }else{
+//     echo "<p>C'est ok !!</p>";
+// };
+// if($isOk){
+//     echo "<p>C'est ok !!</p>";
+// }else{
+//     echo "<p>C'est pas bon !!!</p>";
+// };
+$result = 0;
+$var1 = 0;
+$var2 = 20;
+$var3 = 1;
+$var4 = 100;
+$var5 = 0;
+$var6 = 1;
+$var7 = 1;
+$var8 = 200;
+// while($var1<=10){
+//     echo("<p>$var1</p>");
+//     $var1++;
+// };
+// while($var5<=20){
+//     echo("<p>$var5</p>");
+//     $var5=$var5*$var2;
+//     $var5++;
+// };
+// while($var4>=10){
+//     echo("<p>$var4</p>");
+//     $var4=$var4*$var3;
+//     $var4--;
+// };
+// while($var3<=10){
+//     echo("<p>$var3</p>");
+//     $var3=$var3+($var3/2);
+
+// };
+// while($var6<=15){
+//     echo("<p>$var6 On y arrive presque.</p>");
+//     $var6++;
+// };
+// while($var2>=1){
+//     echo("<p>$var2 C'est presque bon.</p>");
+//     $var2--;
+// };
+// while($var7<=100){
+//     echo("<p>$var7 On tient le bon bout.</p>");
+//     $var7+=15;
+// };
+// while($var8>=0){
+//     echo("<p>$var8 Enfin!!!!</p>");
+//     $var8-=12;
+// };
+function(){
+ return true;
+};
+function phpCestDlaMerde($phpcestnul){
+    return $phpcestnul;
+   };
+$phpcestnul = "phpcestnul";
+$phpcestnul2 = "phppasbien";
+function phpFranchementYaMieux($phpcestnul, $phpcestnul2){
+    return ($phpcestnul+$phpcestnul2);
+};
+function phpCestFatiguant($var4, $var8){
+    if($var4>$var8){
+        echo ("Le premier nombre est plus grand");
+    }else if($var4<$var8){
+        echo ("Le premier nombre est plus petit");
+    }else{
+        echo("Les deux nombres sont identiques");
+    };
+};
+function phpFranchementYaJaimePas($phpcestnul, $var3){
+    return ($phpcestnul+$var3);
+    };
+function AlexandreTesFatiguant($gender, $age){
+        if($genre === "Homme" & $age>17){
+            echo ("Vous êtes un homme et vous êtes majeur");
+        }else if($genre !== "Homme" & $age<18){
+            echo ("Vous êtes une femme et vous êtes mineur");
+        }else if($genre === "Homme" & $age<18){
+            echo("Vous êtes un homme et vous êtes mineur");
+        }else {
+            echo("Vous êtes une femme et vous êtes majeur");
+        };    
+};
 
 ?>
